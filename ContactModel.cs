15533872af92d2a17d6615ac2587a81097e6d6b9
Joshua Flores <49050743+joshflores@users.noreply.github.com>
2023-01-01 01:44:31 -0800

    Create ContactModel.cs

---
+++ ContactModel.cs
@@ -0,0 +1,8 @@
+namespace Project.Models.Domain {
+    public class ContactForm {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
